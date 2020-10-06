91c651c886cff55d689e410305a1864c1395d470
Gusseth <charleskplotado@gmail.com>
2020-10-06 16:44:15 -0700

    Added set/get functions for alpha threshold

--- ShadowGenerator.h
+++ ShadowGenerator.h
@@ -9,11 +9,13 @@ namespace SAG {
 	class ShadowGenerator
 	{
 	public:
-		// Creates an empty generator
+		// Creates a generator with default values
 		ShadowGenerator(PNG & image);
 
 		// Creates a generator with initialized values
 		ShadowGenerator(PNG & image, double threshold, bool invert = false, double alphaThreshold = 0.25);
+
+		// Destructor, flushes image_
 		~ShadowGenerator();
 
 		// Renders the image_ within the generator
@@ -25,6 +27,10 @@ namespace SAG {
 		// Sets the threshold value
 		bool SetThreshold(double threshold);
 		double GetThreshold();
+
+		// Sets the threshold value
+		bool SetAlphaThreshold(double alphaThreshold);
+		double GetAlphaThreshold();
 
 		// Sets the invert value
 		bool SetInvert(bool invert);
