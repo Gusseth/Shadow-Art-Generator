aaaaf044ecc5119691d7ca95ccc38329cedf8f5f
Gusseth <charleskplotado@gmail.com>
2020-10-10 21:13:45 -0700

    Separated the constructors to make it less crowded in .h

--- Generator.cpp
+++ Generator.cpp
@@ -1,1 +1,16 @@
 #include "Generator.h"
+
+
+namespace SAG {
+	Generator::Generator(PNG& image)
+	{
+		image_ = new PNG(image);
+		invert_ = false;
+	}
+
+	Generator::Generator(PNG& image, bool invert)
+	{
+		image_ = new PNG(image);
+		invert_ = invert;
+	}
+}
